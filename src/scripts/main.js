f78315d663abc9d055448743b372955855838979
szczepaniakmichal <mssk87@gmail.com>
2019-12-22 20:54:53 +0100

    clean

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
+const txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quaerat!';
+const text = document.querySelector('.text');
+const span = document.querySelector('.pulse');
+let intervalIndex = 0;
 
+const typing = () => {
+    const textLength = txt.length;
+    text.textContent += txt[intervalIndex];
+    intervalIndex++;
+    if (intervalIndex === textLength) {
+        clearInterval(intervalIdCounter);
+    }
+};
+
+const pulse = () => {
+    span.classList.toggle('remove_pulse');
+};
+
+let intervalIdCounter = setInterval(typing, 100);
+setInterval(pulse, 600);
